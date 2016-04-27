c4f9bc7d46919f28ed765bc9f63ca25d43b26eac
wfcrgt <wfcrgt@163.com>
2016-04-27 17:12:37 +0800

    Site updated: 2016-04-27 17:12:36

--- js/totop.js
+++ js/totop.js
@@ -1,7 +1,7 @@
 (function($) {
 	// When to show the scroll link
 	// higher number = scroll link appears further down the page
-	var upperLimit = 100;
+	var upperLimit = 10;
 
 	// Our scroll link element
 	var scrollElem = $('#totop');
